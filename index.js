a472d1e2b58da89841a629979ee499bc68d9e0cb
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-04 23:25:46 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+  return cats.push('Ralph');
+};
+
+function destructivelyPrependCat() {
+  return cats.unshift('Bob');
+};
+
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+};
+
+function appendCat() {
+  const newAppendCats = [...cats, "Broom"];
+  return newAppendCats;
+};
+
+function prependCat() {
+  const newPrependCats = ["Arnold", ...cats];
+  return newPrependCats;
+};
+
+function removeLastCat() {
+  const lastCat = cats.slice(0, cats.length - 1);
+  return lastCat;
+};
+
+function removeFirstCat() {
+  const firstCat = cats.slice(1);
+  return firstCat;
+}; 
